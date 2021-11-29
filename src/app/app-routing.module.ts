472ad6969b221e06cb5b65ef9159154bd22d69ed
Буков Александр Викторович (DigitalSpirit) <abukov@digital-spirit.ru>
2021-11-29 23:16:19 +0300

    rsa работает

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SiteLayoutComponent } from './shared/layout/site-layout/site-layout.component'
+import { AuthLayoutComponent } from './shared/layout/auth-layout/auth-layout.component'
+import { LoginPageComponent } from './login-page/login-page.component'
+import { AuthGuard } from './shared/classes/auth.guard';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { OfficePageComponent } from './office-page/office-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent, children: [
+        {path: '', redirectTo:'/login', pathMatch:'full'},
+        {path: 'login', component: LoginPageComponent}
+    ]
+  },
+  {
+    path: '', component: SiteLayoutComponent, canActivate: [AuthGuard] ,children: [
+      {path: 'overview', component: OverviewPageComponent},
+      {path: 'post', component: PostPageComponent},
+      {path: 'office', component: OfficePageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layout/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layout/site-layout/site-layout.component';
+import { TokenInterceptor } from './shared/classes/token.interceptor';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { OfficePageComponent } from './office-page/office-page.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    OverviewPageComponent,
+    PostPageComponent,
+    OfficePageComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ {
+    provide: HTTP_INTERCEPTORS,
+    multi: true,
+    useClass: TokenInterceptor
+  }  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
